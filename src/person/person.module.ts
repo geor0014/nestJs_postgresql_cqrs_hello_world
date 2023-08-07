e8610f197f88e38cb93405fd7624b7630add6f61
geor0014 <geor0014@hz.nl>
2023-08-07 10:06:22 +0200

    5 - fixing import error

--- src/person/person.module.ts
+++ src/person/person.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PersonController } from './person.controller';
 import { CqrsModule } from '@nestjs/cqrs';
+import { GetPersonsHandler } from './queries/handler/get-persons.handler/get-persons.handler';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Person } from 'src/entities/person/person';
 
 @Module({
-  imports: [CqrsModule],
+  imports: [CqrsModule, TypeOrmModule.forFeature([Person])],
   controllers: [PersonController],
+  providers: [GetPersonsHandler],
 })
 export class PersonModule {}
