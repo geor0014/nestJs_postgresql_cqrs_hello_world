faa47e7adc38a29970a431af2fd58f5017f89ad2
geor0014 <geor0014@hz.nl>
2023-08-07 09:43:35 +0200

    2 - add postgres connection to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Person } from './entities/person/person';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5435,
+      username: 'postgres', // default
+      password: 'postgres', // default
+      database: 'testDB', // default
+      entities: [Person],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/entities/person/person.ts
+++ src/entities/person/person.ts
@@ -1,1 +1,13 @@
-export class Person {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'person' })
+export class Person {
+  @PrimaryGeneratedColumn('increment', { name: 'id' })
+  id: number;
+
+  @Column({ name: 'name' })
+  name: string;
+
+  @Column({ name: 'age' })
+  age: number;
+}
