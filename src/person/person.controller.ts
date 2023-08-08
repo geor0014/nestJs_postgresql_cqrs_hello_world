ddaf5114b2fe816aec919cf15634142df63e5e99
geor0014 <geor0014@hz.nl>
2023-08-08 08:15:54 +0200

    8 - add pipe to person controller to transform data to DTO

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
-import { QueryBus } from '@nestjs/cqrs';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { GetPersonsQuery } from './queries/implementation/get-persons.query/get-persons.query';
+import { CreatePersonCommand } from './commands/implementation/create-person.command/create-person.command';
 
 @Controller('person')
 export class PersonController {
-  constructor(private qeuryBus: QueryBus) {}
+  constructor(private qeuryBus: QueryBus, private commandBus: CommandBus) {}
 
   @Get()
   async getAll() {
     return await this.qeuryBus.execute(new GetPersonsQuery());
+  }
+
+  @Post()
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  )
+  async create(@Body() payload: CreatePersonCommand) {
+    return await this.commandBus.execute(payload);
   }
 }
