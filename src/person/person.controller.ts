eb6c5a69e0539a978f991be0d222c45ff5158563
geor0014 <geor0014@hz.nl>
2023-08-08 07:58:56 +0200

    6 - add person controller logic

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
+import { GetPersonsQuery } from './queries/implementation/get-persons.query/get-persons.query';
 
 @Controller('person')
-export class PersonController {}
+export class PersonController {
+  constructor(private qeuryBus: QueryBus) {}
+
+  @Get()
+  async getAll() {
+    return await this.qeuryBus.execute(new GetPersonsQuery());
+  }
+}
